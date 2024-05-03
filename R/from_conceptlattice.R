dcb58678a004b0e08442b0a34a69cdbcd4a1a4e1
Domingo <dominlopez@uma.es>
2024-05-03 20:50:18 +0200

    Fix

--- R/from_conceptlattice.R
+++ R/from_conceptlattice.R
@@ -1,28 +1,45 @@
+#' Create a lattice from a concept lattice.
+#'
+#' @param concepts A concept lattice object.
+#'
+#' @return A lattice object.
+#' @export
 from_conceptlattice <- function(concepts) {
 
   if (requireNamespace("fcaR")) {
 
+    # Get the extents of the concepts.
     extents <- concepts$extents()
-    order <- fcaR:::.subset(extents) |>
-      t() |>
+
+    # Get the order of the lattice.
+    order <- fcaR:::.subset(extents) %>%
+      t() %>%
       as.matrix()
 
+    # Get the elements of the lattice.
     els <- c()
     for (i in seq(concepts$size())) {
 
+      # Get the intent of the concept.
       S <- concepts$sub(i)$get_intent()
+
+      # Convert the intent to a string.
       els <- c(els,
                fcaR:::.set_to_string(
                  S$get_vector(),
                  S$get_attributes())
       )
-
     }
 
+    # Set the column and row names of the order to the elements.
     colnames(order) <- rownames(order) <- els
 
+    # Return the lattice.
     return(Lattice$new(order))
-
   }
-
 }
+
+
+
+
+
